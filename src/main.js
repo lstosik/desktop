81ccb356e099c94405b82a81924a5ab80a2a12a4
Łukasz Stosik <lstosik@gmail.com>
2020-06-22 19:25:32 +0200

    Leftover uncommited files

--- src/main.js
+++ src/main.js
@@ -605,7 +605,7 @@ app.on('ready', () => {
     if (config.useSpellChecker) {
       spellChecker = new SpellChecker(
         config.spellCheckerLocale,
-        path.resolve(app.getAppPath(), 'assets/dicts/'),
+        './src/assets/dicts/',
         (err) => {
           if (err) {
             console.error(err);
